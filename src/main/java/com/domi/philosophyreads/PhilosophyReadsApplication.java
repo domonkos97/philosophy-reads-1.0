4666a8cd47e373dec52092b71f41ea0cc6ca3352
Domonkos Szabo <domi@domi.ubuntu>
2023-04-26 11:37:01 +0200

    add spring project

---
+++ src/main/java/com/domi/philosophyreads/PhilosophyReadsApplication.java
@@ -0,0 +1,13 @@
+package com.domi.philosophyreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilosophyReadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilosophyReadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domi/philosophyreads/PhilosophyReadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domi.philosophyreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhilosophyReadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
