07f89bd73a9e8a04883c07d19167655c6894142a
Domonkos Szabo <szabo.domonkos97@gmail.com>
2023-04-26 14:59:00 +0200

    create book service

---
+++ src/main/java/com/domi/philosophyreads/service/BookService.java
@@ -0,0 +1,15 @@
+package com.domi.philosophyreads.service;
+
+import com.domi.philosophyreads.presistance.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+}
