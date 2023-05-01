1b8462b75715bb0ae59742eba762f451337fe631
Domonkos Szabo <szabo.domonkos97@gmail.com>
2023-05-01 21:52:25 +0200

    fix: author renaming #7

--- src/main/java/com/domi/philosophyreads/controller/AuthorController.java
+++ src/main/java/com/domi/philosophyreads/controller/AuthorController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/author")
+@RequestMapping("/api/authors")
 public class AuthorController {
     private final AuthorService authorService;
 
