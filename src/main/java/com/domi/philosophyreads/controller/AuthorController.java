fb1cb5972406de8605afc1e4fb46a482a83c640e
Domonkos Szabo <szabo.domonkos97@gmail.com>
2023-05-01 20:50:09 +0200

    implement add author controller method #7

--- src/main/java/com/domi/philosophyreads/controller/AuthorController.java
+++ src/main/java/com/domi/philosophyreads/controller/AuthorController.java
@@ -1,12 +1,10 @@
 package com.domi.philosophyreads.controller;
 
 import com.domi.philosophyreads.entities.Author;
+import com.domi.philosophyreads.entities.dto.AuthorDto;
 import com.domi.philosophyreads.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +24,10 @@ public class AuthorController {
     @GetMapping("/{id}")
     public Author getAuthorById(@PathVariable Long id) {
         return authorService.getAuthorById(id);
+    }
+
+    @PostMapping("/add")
+    public void addAuthor(@RequestBody AuthorDto authorDto){
+        authorService.addAuthor(authorDto);
     }
 }
