27c2e772b8af4a14eb3e88b007d92c17692a7676
Domonkos Szabo <szabo.domonkos97@gmail.com>
2023-05-02 15:30:02 +0200

    extend user entity with books collection field + override toString #10

--- src/main/java/com/domi/philosophyreads/entities/User.java
+++ src/main/java/com/domi/philosophyreads/entities/User.java
@@ -1,10 +1,10 @@
 package com.domi.philosophyreads.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.List;
+
 @Entity
 @Data
 @Table( name = "_user")
@@ -14,4 +14,12 @@ public class User {
     private Long id;
     private String userName;
     private String password;
+    @OneToMany
+    private List<Book> books;
+
+    public String toString(){
+        return "User{" +'\n' +
+                "id=" + id + "," + '\n' +
+                "userName=" + userName + "}";
+    }
 }
