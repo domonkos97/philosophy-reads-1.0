e0dcb28f8b163968e79621d8a5d8863d8a58af5c
Domonkos Szabo <szabo.domonkos97@gmail.com>
2023-05-02 15:40:20 +0200

    implement usercontoller #10

--- src/main/java/com/domi/philosophyreads/controller/UserController.java
+++ src/main/java/com/domi/philosophyreads/controller/UserController.java
@@ -1,2 +1,19 @@
-package com.domi.philosophyreads.controller;public class UserController {
+package com.domi.philosophyreads.controller;
+
+import com.domi.philosophyreads.entities.User;
+import com.domi.philosophyreads.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    public final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
 }
