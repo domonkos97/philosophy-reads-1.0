6ee56162a60a55e10bfcf422607b5e0b6937f2b8
Domonkos Szabo <szabo.domonkos97@gmail.com>
2023-05-02 15:48:47 +0200

    implement getAllUsers #10

--- src/main/java/com/domi/philosophyreads/controller/UserController.java
+++ src/main/java/com/domi/philosophyreads/controller/UserController.java
@@ -3,8 +3,11 @@ package com.domi.philosophyreads.controller;
 import com.domi.philosophyreads.entities.User;
 import com.domi.philosophyreads.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -14,6 +17,11 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
     }
 
 }
--- src/main/java/com/domi/philosophyreads/service/UserService.java
+++ src/main/java/com/domi/philosophyreads/service/UserService.java
@@ -3,7 +3,10 @@ package com.domi.philosophyreads.service;
 import com.domi.philosophyreads.entities.User;
 import com.domi.philosophyreads.presistance.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -12,5 +15,10 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        Sort sortByTitleDesc = Sort.by(Sort.Direction.DESC, "userName");
+        return userRepository.findAll();
     }
 }
