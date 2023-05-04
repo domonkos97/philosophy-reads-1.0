ed461537c1c2e4e57ae6c87203cf0abab7a4b7b9
Domonkos Szabo <szabo.domonkos97@gmail.com>
2023-05-04 13:09:22 +0200

    clean up loginAuthFilter #10

--- src/main/java/com/domi/philosophyreads/security/LoginAuthFilter.java
+++ src/main/java/com/domi/philosophyreads/security/LoginAuthFilter.java
@@ -1,46 +1,34 @@
 package com.domi.philosophyreads.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.auth0.jwt.JWT;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.security.core.GrantedAuthority;
+import com.auth0.jwt.algorithms.Algorithm;
+import static com.auth0.jwt.JWT.*;
 
 public class LoginAuthFilter extends UsernamePasswordAuthenticationFilter {
-
-    /**
-        The AuthenticationManager interface in Spring Security is responsible for authenticating an Authentication object,
-        which represents the user's credentials.
-     */
-    private final AuthenticationManager authenticationManager;
-    public LoginAuthFilter(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
-    }
-
-    /**
-        The attemptAuthentication method is responsible for attempting to authenticate the user
-        with the given credentials (i.e. username and password)
-        and returning an Authentication object representing the authenticated user on success.
-        If the authentication fails, it will throw an AuthenticationException.
-     */
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        //
-        throw new UnsupportedOperationException("method not implemented yet");
-    }
     /**
      The successfulAuthentication method is called after the attemptAuthentication method successfully authenticates the user.
      It is responsible for creating the JWT token, setting it in the response cookie, and sending the response to the client.
      */
     @Override
     public void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException {
-        throw new UnsupportedEncodingException("method not implemented yet");
+        List<String> authorityList = authResult.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
+        String secret_key = System.getenv("JWT_SECRET");
+        Algorithm algorithm = Algorithm.HMAC256(secret_key);
+        String token = JWT.create()
+                .withSubject(authResult.getName())
+                .withIssuer(request.getRequestURL().toString())
+                .withClaim("role", authorityList)
+                .sign(algorithm);
     }
 }
